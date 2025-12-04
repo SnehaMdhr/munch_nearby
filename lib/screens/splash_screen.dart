a3dfcbb8ccd24fe05b2e468e18028035abe02ee6
sneha <snehamanandhar54@gmail.com>
2025-12-04 23:21:54 +0545

    some changes in splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'onboarding_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 5), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const OnboardingScreen(),
+          ),
+        );
+      });
+    });
+    return Scaffold(
+        body: SizedBox.expand(
+        child: Image.asset(
+        "assets/images/logo.png",
+        fit: BoxFit.cover,
+    ),),
+    );
+  }
+}
