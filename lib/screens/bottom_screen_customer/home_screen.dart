6ef7e6e692518e9f29276d7afaf15269c73e588b
sneha <snehamanandhar54@gmail.com>
2025-12-18 21:42:18 +0545

    dashboard for customer compeleted

--- lib/screens/bottom_screen_customer/home_screen.dart
+++ lib/screens/bottom_screen_customer/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:munch_nearby/widgets/top_search_bar.dart';
 
+import '../../widgets/restaurant_card.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -11,7 +13,34 @@ class HomeScreen extends StatelessWidget {
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TopSearchBar()
+              TopSearchBar(),
+              const SizedBox(height: 20),
+
+              const RestaurantCard(
+                image: "assets/images/chiya.png",
+                name: "Chiya Aada",
+                rating: 4.9,
+                type: "Cafe",
+                distance: "0.3 mi",
+              ),
+              const SizedBox(height: 20),
+
+              const RestaurantCard(
+                image: "assets/images/chiya.png",
+                name: "Bella Luna",
+                rating: 4.8,
+                type: "Italian",
+                distance: "0.5 mi",
+              ),
+              const SizedBox(height: 20),
+
+              const RestaurantCard(
+                image: "assets/images/chiya.png",
+                name: "Green Leaf",
+                rating: 4.7,
+                type: "Vegan",
+                distance: "0.6 mi",
+              ),
             ]
         ),
       ))
---
+++ lib/widgets/restaurant_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class RestaurantCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final double rating;
+  final String type;
+  final String distance;
+
+  const RestaurantCard({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.rating,
+    required this.type,
+    required this.distance,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(16),
+            ),
+            child: Image.asset(
+              image,
+              height: 160,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                Row(
+                  children: [
+                    const Icon(Icons.star,
+                        color: Colors.orange, size: 18),
+                    const SizedBox(width: 5),
+                    Text("$rating"),
+                    const SizedBox(width: 10),
+                    Text(
+                      "| $type | $distance",
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
