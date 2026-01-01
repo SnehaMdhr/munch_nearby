9c924970d63be8922520b3c9125a835b233a4cb2
sneha <snehamanandhar54@gmail.com>
2026-01-01 23:24:15 +0545

    added auth repository with provider

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:munch_nearby/core/error/failure.dart';
+import 'package:munch_nearby/features/auth/data/datasources/auth_datasource.dart';
+import 'package:munch_nearby/features/auth/domain/entities/auth_entity.dart';
+import 'package:munch_nearby/features/auth/domain/repositories/auth_repository.dart';
+import '../datasources/local/auth_local_datasource.dart';
+import '../models/auth_hive_model.dart';
+
+final authRepositoryProvider = Provider<IAuthRepository>((ref){
+  return AuthRepository(authDatasource: ref.read(authLocalDatasourceProvider));
+});
+
+class AuthRepository implements IAuthRepository{
+  final IAuthDatasource _authDatasource;
+  AuthRepository({required IAuthDatasource authDatasource})
+    : _authDatasource = authDatasource;
+
+  @override
+  Future<Either<Failure, AuthEntity>> login(String email, String password) async{
+    try{
+      final user = await _authDatasource.login(email, password);
+      if(user !=null){
+        final entity = user.toEntity();
+        return Right(entity);
+      }
+      return Left(LocalDatabaseFailure(message: "Invalid email or password"));
+    }catch(e){
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> register(AuthEntity entity) async{
+    try{
+      final model = AuthHiveModel.fromEntity(entity);
+      final result = await _authDatasource.register(model);
+      if(result){
+        return Right(true);
+      }
+      return Left(LocalDatabaseFailure(message: "Failed to register user"));
+    }catch(e){
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+}
+
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,7 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:munch_nearby/core/error/failure.dart';
 import 'package:munch_nearby/features/auth/domain/entities/auth_entity.dart';
 
-abstract interface class IAuthRepository{
+abstract interface class IAuthRepository {
   Future<Either<Failure, bool>> register(AuthEntity entity);
-  Future<Either<Future,AuthEntity>> login(String email, String password);
+
+  Future<Either<Failure, AuthEntity>> login(String email, String password);
 }
