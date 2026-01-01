b30482751c7a6bc024f4e469362624e468fa2c42
sneha <snehamanandhar54@gmail.com>
2026-01-01 22:51:09 +0545

    hive service added and imported in main.dart

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,58 @@
+
+import 'package:hive/hive.dart';
+import '../../../features/auth/data/models/auth_hive_model.dart';
+import '../../constants/hive_table_constant.dart';
+import 'package:path_provider/path_provider.dart';
+class HiveService {
+  //init
+  Future<void> init() async{
+    final directory = await getApplicationDocumentsDirectory();
+    final path = "${directory.path}/${HiveTableConstant.dbName}";
+    Hive.init(path);
+    _registerAdapter();
+    await openBoxes();
+  }
+
+  //register adapter
+  void _registerAdapter(){
+    if(!Hive.isAdapterRegistered(HiveTableConstant.userTypeId)){
+      Hive.registerAdapter(AuthHiveModelAdapter());
+    }
+  }
+  //Open boxes
+  Future<void> openBoxes() async{
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.userTable);
+  }
+  //close boxes
+  Future <void> close() async{
+    await Hive.close();
+  }
+  //Queries
+
+
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.userTable);
+
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async{
+    await _authBox.put(model.userId, model);
+    return model;
+  }
+
+  Future<AuthHiveModel?> loginUser(String email, String password)async{
+    final users = _authBox.values.where(
+          (user) => user.email == email && user.password == password,
+    );
+    if(users.isNotEmpty){
+      return users.first;
+    }
+    return null;
+  }
+
+  bool isEmailExists(String email){
+    final users = _authBox.values.where((user)=> user.email == email);
+    return users.isNotEmpty;
+  }
+
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:munch_nearby/app/app.dart';
 
-void main(){
+import 'core/services/hive/hive_service.dart';
+
+Future<void> main() async {
+  await HiveService().init();
   runApp(App());
 }
