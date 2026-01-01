07760faa503f3729d35ec1bfb915e9a232948e9d
sneha <snehamanandhar54@gmail.com>
2026-01-01 22:57:44 +0545

    provider added in hive services

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -1,8 +1,13 @@
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import '../../../features/auth/data/models/auth_hive_model.dart';
 import '../../constants/hive_table_constant.dart';
 import 'package:path_provider/path_provider.dart';
+
+final hiveServiceProvider = Provider<HiveService>((ref){
+  return HiveService();
+});
 class HiveService {
   //init
   Future<void> init() async{
