5afe30106fa0a52124b8740ed5509ca71873d36f
sneha <snehamanandhar54@gmail.com>
2026-01-01 23:48:46 +0545

    auth state and view model added

---
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -0,0 +1,78 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:munch_nearby/features/auth/domain/entities/auth_entity.dart';
+
+import '../../domain/usecases/login_usecase.dart';
+import '../../domain/usecases/register_usecase.dart';
+import '../state/auth_state.dart';
+final authViewModelProvider = NotifierProvider<AuthViewModel, AuthState>(
+      () => AuthViewModel(),
+);
+
+class AuthViewModel extends Notifier<AuthState>{
+  late final RegisterUsecase _registerUsecase;
+  late final LoginUsecase _loginUsecase;
+
+  @override
+  AuthState build() {
+    _registerUsecase = ref.read(registerUsecaseProvider);
+    _loginUsecase= ref.read(loginUsecaseProvider);
+    return AuthState();
+  }
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required UserRole role,
+    required String username,
+    required String password,
+  })async{
+    state = state.copyWith(status: AuthStatus.loading);
+    await Future.delayed(Duration(seconds: 2));
+    final params = RegisterUsecaseParams(
+        name: name,
+        email: email,
+        role: role,
+        username: username,
+        password: password
+    );
+    final result = await _registerUsecase(params);
+    result.fold(
+          (failure){
+        state= state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+          (isRegistered){
+        if(isRegistered){
+          state=state.copyWith(status: AuthStatus.registered);
+        }
+      },
+    );
+  }
+
+  Future<void> login({
+    required String email,
+    required String password,
+  })async{
+    state = state.copyWith(status: AuthStatus.loading);
+    final params = LoginUsecaseParams(email: email, password: password);
+    final result = await _loginUsecase(params);
+
+    result.fold(
+          (failure){
+        state=state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+          (authEntity){
+        state=state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
+      },
+    );
+  }
+
+}
