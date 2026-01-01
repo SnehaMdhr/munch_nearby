e0664fa3e48ceb5e6b5eed679a01cd238f3704f6
sneha <snehamanandhar54@gmail.com>
2026-01-01 23:06:22 +0545

    added auth datasource and local datasource

---
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -0,0 +1,9 @@
+
+import '../models/auth_hive_model.dart';
+
+abstract interface class IAuthDatasource{
+  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel?> login(String email, String password);
+
+  Future<bool> isEmailExists(String email);
+}
---
+++ lib/features/auth/data/datasources/local/auth_local_datasource.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../core/services/hive/hive_service.dart';
+import '../../models/auth_hive_model.dart';
+import '../auth_datasource.dart';
+
+final authLocalDatasourceProvider = Provider<AuthLocalDatasource>((ref){
+  final hiveService = ref.watch(hiveServiceProvider);
+  return AuthLocalDatasource(hiveService: hiveService);
+});
+
+class AuthLocalDatasource implements IAuthDatasource{
+  final HiveService _hiveService;
+
+  AuthLocalDatasource({required HiveService hiveService})
+      : _hiveService =hiveService;
+
+  @override
+  Future<bool> isEmailExists(String email) {
+    try{
+      final exists = _hiveService.isEmailExists(email);
+      return Future.value(exists);
+    }catch(e){
+      return Future.value(false);
+    }
+  }
+
+  @override
+  Future<AuthHiveModel?> login(String email, String password)async {
+    try{
+      final user = await _hiveService.loginUser(email, password);
+      return Future.value(user);
+    }catch(e){
+      return Future.value(null);
+    }
+  }
+
+  @override
+  Future<bool> register(AuthHiveModel model) async{
+    try{
+      await _hiveService.registerUser(model);
+      return Future.value(true);
+    }catch(e){
+      return Future.value(false);
+    }
+  }
+
+}
