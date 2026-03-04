bdf41fa1b3b517ef3b66273ce0b948d967fe6ae6
sneha <snehamanandhar54@gmail.com>
2026-03-04 08:54:18 +0545

    animation added in splash screen

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -12,37 +12,126 @@ class SplashScreen extends ConsumerStatefulWidget {
   ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends ConsumerState<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _taglineSlide;
 
   @override
   void initState() {
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.8,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutBack));
+
+    _taglineSlide = Tween<double>(
+      begin: 20,
+      end: 0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
     _navigateToNext();
   }
 
   void _navigateToNext() async {
     await Future.delayed(const Duration(seconds: 3));
-
     if (!mounted) return;
 
     final userSessionService = ref.read(userSessionServiceProvider);
     final isLoggedIn = userSessionService.isLoggedIn();
 
     if (isLoggedIn) {
-      AppRoutes.pushReplacement(context, const BottomNavigationBarForCustomer());
+      AppRoutes.pushReplacement(
+        context,
+        const BottomNavigationBarForCustomer(),
+      );
     } else {
       AppRoutes.pushReplacement(context, const OnboardingScreen());
     }
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox.expand(
-        child: Image.asset(
-          "assets/images/logo.png",
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        children: [
+          // Background image
+          SizedBox.expand(
+            child: Image.asset(
+              "assets/images/logo_background.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: ScaleTransition(
+                    scale: _scaleAnimation,
+                    child: Image.asset(
+                      "assets/images/logo_without_background.png",
+                      width: 300,
+                      height: 300,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    return Transform.translate(
+                      offset: Offset(0, _taglineSlide.value),
+                      child: Opacity(
+                        opacity: _fadeAnimation.value,
+                        child: child,
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Discover Delicious Food Nearby",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54,
+                      shadows: [
+                        Shadow(
+                          color: Colors.grey,
+                          offset: Offset(1, 1),
+                          blurRadius: 4,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
